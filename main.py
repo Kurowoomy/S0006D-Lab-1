966cb16f8c1bbc1db9e941b79f6c17866700827c
Emma Eliasson <gurgelsnurp@hotmail.se>
2020-01-23 19:10:28 +0100

    Write the rest of the states :D

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import enum
 
+
 class location_type(enum.Enum):
     goldMine = 1
     bank = 2
@@ -21,8 +22,8 @@ class BaseGameEntity:
 class Miner(BaseGameEntity):
     def __init__(self, ID):
         BaseGameEntity.__init__(self, ID)
-        self.currentState = State_RunAway()
-        self.location = location_type.homeSweetHome
+        self.currentState = EnterMineAndDigForNugget()
+        self.location = location_type.goldMine
         self.goldCarried = 0
         self.moneyInBank = 0
         self.thirst = 0
@@ -33,27 +34,53 @@ class Miner(BaseGameEntity):
         self.currentState.Execute(self)
 
     def changeState(self, newState):
-        self.currentState = newState
-        print(self.currentState)
-    
+        self.currentState.Exit(self)
+        self.currentState = newState()
+        self.currentState.Enter(self)
+
+    def changeLocation(self, newLocation):
+        self.location = newLocation
+
 
 
 class State:
-    def Execute(self, character):
-        print(character.ID)
-
-class State_RunAway(State):
-    def Execute(self, character):
-        print(character.ID, 'is running away')
-
-class State_Sleep(State):
-    #TODO: add transitions and their conditions in the state classes
     pass
+
+class EnterMineAndDigForNugget(State):
+    def Enter(self, character):
+        if(character.location != location_type.goldMine):
+            print('Walkin to the gold mine')
+            character.changeLocation(location_type.goldMine)
+    def Execute(self, character):
+        character.goldCarried += 1
+        character.fatigue += 1
+        print("Pickin' up a nugget")
+        if(character.goldCarried >= 3):
+            character.changeState(VisitBankAndDepositGold)
+        if(character.thirst >= 5):
+            character.changeState(QuenchThirst)
+    def Exit(self, character):
+        print("Ah'm leavin' the gold mine with mah pockets full o' gold")
+
+class VisitBankAndDepositGold(State):
+    def Enter(self, character):
+        pass
+    def Execute(self, character):
+        pass
+    def Exit(self, character):
+        pass
+
+class QuenchThirst(State):
+    def Execute(self, character):
+        pass
+
+class GoHomeAndSleepTilRested(State):
+    def Execute(self, character):
+        pass
 
 #run()
 miner1 = Miner(0)
-miner2 = Miner(1)
-
-print(miner1.ID, miner2.ID, BaseGameEntity.nextValidID)
-
-
+i = 0
+while(i < 6):
+    i += 1
+    miner1.update()
